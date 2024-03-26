c8508275b00bf3e02cba2db14a8437d6ae69b4e7
benjamink <benjamink@mirageoscience.com>
2024-03-26 11:34:19 -0700

    fix mypy in configuratio file i guess

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,23 +6,22 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'las-geoh5'
-copyright = '2024, Ben Kary'
-author = 'Ben Kary'
-release = '2024'
+project = "las-geoh5"
+copyright = "2024, Ben Kary"
+author = "Ben Kary"
+release = "2024"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions: list[str] = []
 
-templates_path = ['_templates']
-exclude_patterns = []
-
+templates_path = ["_templates"]
+exclude_patterns: list[str] = []
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
