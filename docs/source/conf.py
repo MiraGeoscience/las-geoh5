6ce3721df14d28b7e4874d0ccb6aac80140e0798
dominiquef <dominiquef@mirageoscience.com>
2024-08-06 15:24:01 -0700

    Change versions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,6 +27,6 @@ html_theme = "alabaster"
 html_static_path = ["_static"]
 
 # The short X.Y version.
-version = "0.2.0"
+version = "0.2.1"
 # The full version, including alpha/beta/rc tags.
-release = "0.2.0"
+release = "0.2.1-alpha.1"
