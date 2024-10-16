204e161ca29f01df133c4cc8ccc3ace6beb1b688
dominiquef <dominiquef@mirageoscience.com>
2024-10-16 11:02:21 -0700

    Temp disable pylint warnings

--- las_geoh5/import_las.py
+++ las_geoh5/import_las.py
@@ -288,7 +288,7 @@ def add_data(
     return drillhole
 
 
-def create_or_append_drillhole(
+def create_or_append_drillhole(  # pylint: disable=too-many-locals
     lasfile: lasio.LASFile,
     drillhole_group: DrillholeGroup,
     group_name: str,
@@ -350,7 +350,7 @@ def create_or_append_drillhole(
     return drillhole
 
 
-def las_to_drillhole(
+def las_to_drillhole(  # pylint: disable=too-many-locals
     data: lasio.LASFile | list[lasio.LASFile],
     drillhole_group: DrillholeGroup,
     property_group: str,
